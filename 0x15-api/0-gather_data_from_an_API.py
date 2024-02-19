12419b53a9ed00170b1e143010edc7ddf120753b
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-19 09:40:24 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Script that uses REST API and takes ID as arg to return TODO info """
-import json
-from urllib import parse
-import sys
-from urllib.request import urlopen
-""" imports several libraries"""
 
 if __name__ == "__main__":
     """ ensures that code will not run if imported """
+    import json
+    from urllib import parse
+    import sys
+    from urllib.request import urlopen
+    """ imports several libraries"""
+
     try:
         if len(sys.argv) != 2:
             raise ValueError("Usage: python script.py <employee_id>")
@@ -23,12 +24,13 @@ if __name__ == "__main__":
         TOTAL_NUMBER_OF_TASKS = len(todo_item)
         NUMBER_OF_DONE_TASKS = sum(1 for task in todo_list if task['completed'])
 
-        print("Employee {} is done with tasks ({}/{})"
-                .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,TOTAL_NUMBER_OF_TASKS)
+        print("Employee {} is done with tasks ({}/{})".
+            format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
 
         for task in todo_item:
-            if task['completed']:
-                print("\t {}".format('TASK_TITLE')
+        TASK_TITLE = todo_item.get('title')
+        if todo_item.get('completed'):
+            print("\t {}".format('TASK_TITLE')
 
     except Exception as e:
         pass
