9ad75a448c0305d15f8b97ba4556364414cf14bb
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-19 14:25:37 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,7 +4,6 @@
 if __name__ == "__main__":
     """ ensures that code will not run if imported """
     import json
-    from urllib import parse
     import sys
     from urllib.request import urlopen
     """ imports several libraries"""
@@ -17,19 +16,20 @@ if __name__ == "__main__":
 
         url = f'https://jsonplaceholder.typicode.com/todos?userId={employe_id}'
         with urlopen(url) as response:
-            displayed_value = response.read()
-            todo_item = json.loads(displayed_value)
+            body = response.read()
+            todo_item = json.loads(body)
 
         EMPLOYEE_NAME = todo_item[0]['username']
         TOTAL_NUMBER_OF_TASKS = len(todo_item)
-        NUMBER_OF_DONE_TASKS = sum(1 for task in todo_list if task['completed'])
+        NUMBER_OF_DONE_TASKS = sum(task['completed']
+                for task in todo_item)
 
-        print("Employee {} is done with tasks ({}/{})".
+        print("Employee {} is done with tasks ({}/{}):".
             format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
 
         for task in todo_item:
-        TASK_TITLE = todo_item.get('title')
-        if todo_item.get('completed'):
+        TASK_TITLE = task.get('title')
+        if task.get('completed'):
             print("\t {}".format('TASK_TITLE')
 
     except Exception as e:
