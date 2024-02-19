4d7f45230875038ae08474cad0fd12077997a5c4
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-19 15:21:36 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,10 +3,8 @@
 
 if __name__ == "__main__":
     """ ensures that code will not run if imported """
-    import json
     import sys
-    from urllib.request import urlopen
-    """ imports several libraries"""
+    import requests
 
     try:
         if len(sys.argv) != 2:
@@ -15,22 +13,20 @@ if __name__ == "__main__":
         employe_id = int(sys.argv[1])
 
         url = f'https://jsonplaceholder.typicode.com/todos?userId={employe_id}'
-        with urlopen(url) as response:
-            body = response.read()
-            todo_item = json.loads(body)
+        response = requests.get(url)
+        todo_item = response.json()
 
         EMPLOYEE_NAME = todo_item[0]['username']
         TOTAL_NUMBER_OF_TASKS = len(todo_item)
-        NUMBER_OF_DONE_TASKS = sum(task['completed']
-                for task in todo_item)
+        NUMBER_OF_DONE_TASKS = sum(task['completed'] for task in todo_item)
 
-        print("Employee {} is done with tasks ({}/{}):".
-            format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
+        print("Employee {} is done with tasks ({}/{}):"
+             .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
         for task in todo_item:
-        TASK_TITLE = task.get('title')
-        if task.get('completed'):
-            print("\t {}".format('TASK_TITLE')
+            TASK_TITLE = task.get('title')
+            if task.get('completed'):
+                print("\t{}".format(TASK_TITLE))
 
     except Exception as e:
-        pass
+        print(f"Error: {e}")
