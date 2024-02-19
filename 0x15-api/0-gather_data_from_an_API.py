e6bdf9e31128e7efc956c70e50cade33a48ebff0
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-19 08:53:54 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,15 +8,21 @@ import json
 
 if __name__ == "__main__":
     """ ensures that code will not run if imported """
-    try:
-        int_value = sys.argv[1]
-        url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+        employee_id = int(sys.argv[1])
+        
+        url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
         with urlopen(url) as response:
             displayed_value = response.read()
 
-        todo_item = json.loads.(displayed_value)
-    a = 'Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):'
-    print a
-    print\t\s('TASK_TITLE')
+            todo_item = json.loads(displayed_value)
+
+        EMPLOYEE_NAME = todo_item[0]['username']
+        TOTAL_NUMBER_OF_TASKS = len(todo_item)
+        NUMBER_OF_DONE_TASKS = sum(1 for task in todo_list if task['completed'])
+
+    print("Employee {} is done with tasks ({}/{})"
+            .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,TOTAL_NUMBER_OF_TASKS)
+    print\t('TASK_TITLE')
     except:
         pass
