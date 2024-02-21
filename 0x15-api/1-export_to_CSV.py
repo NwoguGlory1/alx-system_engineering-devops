74f51ffde7ed0bd46c3d27445a2330e3df022404
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-21 08:47:30 -0800

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,20 +2,18 @@
 """ Gather data from an API  """
 
 if __name__ == "__main__":
-    import csv
     from requests import get
     from sys import argv, exit
-    import sys
 
     try:
-        employ_id = argv[1]
-        is_int = int(employ_id)
+        id = argv[1]
+        is_int = int(id)
     except ValueError:
         exit()
 # retrieves the value of cmdline arg,converts it to int, if any error, it exits
 
-    url_user = "https://jsonplaceholder.typicode.com/users?id=" + employ_id
-    url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + employ_id
+    url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
+    url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
 # amends the api url to get the user info & the associated todo info
 
     r_user = get(url_user)
@@ -29,26 +27,20 @@ if __name__ == "__main__":
     except ValueError:
         print("Not a valid JSON")
 
-    data = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
-
-    for user in js_user:
-        USER_ID = employ_id
-        USERNAME = user.get('username')
+    if js_user and js_todo:
+        USER_ID = id = employee_id
+        USERNAME = js_user[0].get('username')
 
         for todo in js_todo:
             TASK_TITLE = todo.get('title')
             TASK_COMPLETED_STATUS = todo.get("completed")
 
-            TASK_COMPLETED_STATUS = str(TASK_COMPLETED_STATUS)
-            .lower() == 'true'
+        csv_file_name = f"{USER_ID}.csv"
 
-            data.append([USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
 
-    csv_file_name = f"{USER_ID}.csv"
+        with open(csv_file_name, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
 
-    with open(csv_file_name, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(data)
-
-    for record in data[1:]:
+        for record in data[1:]:
         print(','.join(map(lambda x: f'"{x}"', record)))
