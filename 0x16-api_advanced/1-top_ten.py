1eef45106c00924ea2ebbbb18387812166a9872d
NwoguGlory1 <glorynwogu18@gmail.com>
2024-03-05 13:51:32 -0800

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+Defines a function for retrieving
+the titles of the first 10 hot posts listed 
+for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Retrieve the titles of  the first 10 hot posts for a given subreddit.
+
+    Args:
+    - subreddit (str): The name of the subreddit
+
+    Returns:
+    - The the first 10 hot posts or None if the subreddit is invalid.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'
+    .format(subreddit)
+
+    headers = {"User-Agent": "My-User-Agent"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    try:
+        top_ten = response.json()['data']['children']
+        for post in top_ten:
+            print(post['data']['title'])
+    except KeyError:
+        print("None")
+
