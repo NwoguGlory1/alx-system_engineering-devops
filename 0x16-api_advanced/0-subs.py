0cf456a353046dfc68a0f2aeb26fa5c4c834d479
NwoguGlory1 <glorynwogu18@gmail.com>
2024-03-05 12:37:23 -0800

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,17 +14,15 @@ def number_of_subscribers(subreddit):
     - subreddit (str): The name of the subreddit
 
     Returns:
-    - int: The number of subscribers, or 0 if the subreddit is invalid.
+    - The number of subscribers, or 0 if the subreddit is invalid.
     """
-
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
 
     response = requests.get(
-            url,  headers={'User-Agent': 'My-User-Agent'}, allow_redirects=False
-            )
+            url, headers=headers, allow_redirects=False)
 
     if response.status_code >= 300:
         return 0
-    else:
-        data = response.json()
-        return data['data']['subscribers']
+    data = response.json().get('data').get('subscribers')
+    return data
